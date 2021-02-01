dcd6b1cc966979d722eb671282a4a2ae04292eb4
Mohamed <33926500+mohamedamine724@users.noreply.github.com>
2021-02-01 20:49:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<H1>TEST</H1>
+
+$get_data = callAPI('GET', 'https://api.example.com/get_url/'.$user['User']['customer_id'], false);
+$response = json_decode($get_data, true);
+$errors = $response['response']['errors'];
+$data = $response['response']['data'][0];
